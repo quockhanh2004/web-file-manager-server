89c1bd77e139d876a98dcf4d91b35f0dab0640a0
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2024-07-07 13:52:11 +0700

    hoàn thiện

--- server.js
+++ server.js
@@ -15,13 +15,18 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Đặt express.static() trước các route khác 
-app.use(express.static('public')); 
-
-// Route để phục vụ file CSS từ thư mục 'views'
-app.get('/views/style.css', (req, res) => {
-  res.sendFile(__dirname + '/views/style.css');
+app.use(express.static('public'));
+// Route để phục vụ file CSS từ thư mục 'public' (nếu cần)
+app.get('/style.css', (req, res) => {
+  res.sendFile(__dirname + '/views/style.css'); 
 });
-
+// app.use((req, res, next) => {
+//   const ext = path.extname(req.url);
+//   if (ext === '.css') {
+//     res.set('Content-Type', 'text/css');
+//   }
+//   next();
+// });
 // Route để xử lý download file
 app.get('/download/:filename', (req, res) => {
   const filename = req.params.filename;
@@ -39,7 +44,7 @@ app.get('/download/:filename', (req, res) => {
 // Route hiển thị danh sách file và folder
 app.get('/', (req, res) => {
   const currentDir = req.query.dir || '';
-  const directoryPath = path.join(__dirname, 'drive', currentDir);	
+  const directoryPath = path.join(__dirname, 'drive', currentDir);
 
   fs.readdir(directoryPath, (err, items) => {
     if (err) {
@@ -48,21 +53,24 @@ app.get('/', (req, res) => {
     }
 
     const itemsWithDetails = items.map(item => {
-  const itemPath = path.join(directoryPath, item);
-  const stat = fs.statSync(itemPath);
-  return {
-    name: item,
-    extension: getFileExtension(item),
-    isDirectory: stat.isDirectory(),
-    path: path.join(currentDir, item) // Thêm thuộc tính path
-  };
-});
+      const itemPath = path.join(directoryPath, item);
+      const stat = fs.statSync(itemPath);
+      return {
+        name: item,
+        extension: getFileExtension(item),
+        isDirectory: stat.isDirectory(),
+        path: path.join(currentDir, item) // Thêm thuộc tính path
+      };
+    });
 
-res.render('index', { 
-    items: itemsWithDetails, 
-    currentDir, 
-    parentDir: path.dirname(currentDir) // Thêm parentDir vào đây
+    // Sửa lỗi thiếu dấu ngoặc đóng cho `res.render`
+    res.render('index', { 
+      items: itemsWithDetails, 
+      currentDir, 
+      parentDir: currentDir ? path.dirname(currentDir) : '' // Xử lý trường hợp parentDir rỗng
+    }); 
   });
+});
 
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
