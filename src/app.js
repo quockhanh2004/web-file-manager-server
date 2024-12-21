d57290c5c447b04203da4cbaafd41f7e0eb0c8d8
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2024-12-21 16:14:43 +0700

    add 404 not found page

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const path = require('path');
-const fileRoutes = require('./routes/files');
-const morgan = require('morgan'); 
-const moment = require('moment-timezone');
+const express = require("express");
+const path = require("path");
+const fileRoutes = require("./routes/files");
+const morgan = require("morgan");
+const moment = require("moment-timezone");
 
 const app = express();
 const port = 2345;
 
 // Thiết lập EJS và thư mục public
-morgan.token('vn-time', () => {
-  return moment().tz('Asia/Ho_Chi_Minh').format('DD/MM/YYYY HH:mm:ss');
+morgan.token("vn-time", () => {
+  return moment().tz("Asia/Ho_Chi_Minh").format("DD/MM/YYYY HH:mm:ss");
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '..', 'views'));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "..", "views"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use((req, res, next) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   //chỉ log ipv4
   let ipv4;
-  if (ip.includes(':')) {
-    ipv4 = ip.split(':')[3];
+  if (ip.includes(":")) {
+    ipv4 = ip.split(":")[3];
   } else {
     ipv4 = ip;
   }
@@ -31,7 +31,12 @@ app.use((req, res, next) => {
 app.use(morgan(`:vn-time| :method :url status::status :response-time ms`));
 
 // Sử dụng route cho /files
-app.use('/', fileRoutes); 
+app.use("/", fileRoutes);
+
+// Middleware xử lý lỗi 404 cho UI (trang web)
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "..", "public", "404.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
